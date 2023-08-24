945f2bd2ca89c40a3db3348a20445486b1ae23ea
malcolmzed <142301304+malcolmzed@users.noreply.github.com>
2023-08-25 00:16:37 +0100

    all exercies completed

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -3,7 +3,30 @@ package com.techreturners.exercise005;
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
-    }
 
+        String lowerInput = input.toLowerCase();
+        String letters26 = "abcdefghijklmnopqrstuvwxyz";
+
+        int ascii;
+        int offset;
+
+        char aChar = 'a';
+        int aInt = aChar;
+
+        char[] chars = letters26.toCharArray();
+        int counter = 0;
+
+        for (int i = 0; i < lowerInput.length(); i++) {
+            ascii = lowerInput.charAt(i);
+            offset = ascii - aInt;
+
+            if (offset >= 0 && offset < 26 && chars[offset] != '+') {
+                counter++;
+                chars[offset] = '+';
+            }
+
+
+        }
+        return (counter == 26) ?  true : false;
+    }
 }
--- src/test/java/com/techreturners/exercise004/Exercise004Test.java
+++ src/test/java/com/techreturners/exercise004/Exercise004Test.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Exercise004Test {
 
-    
+
     @Test
     public void checkGetDateTimeWhenDateIsSpecified() {
 
